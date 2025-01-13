87dc0452d8daa1e162224fb92ed7074fb5ef055d
Isaac Irwin <isaacirwin@Isaacs-MacBook-Pro-6.local>
2025-01-13 10:29:31 -0700

    Board Finished

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,10 @@
 package chess;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -14,6 +19,7 @@ public class ChessBoard {
         locations = new boolean[8][8];
     }
 
+
     /**
      * Adds a chess piece to the chessboard
      *
@@ -23,7 +29,6 @@ public class ChessBoard {
     public void addPiece(ChessPosition position, ChessPiece piece) {
         board[position.getRow() - 1][position.getColumn() - 1] = piece;
         locations[position.getRow() - 1][position.getColumn() - 1] = true;
-//        throw new RuntimeException("Not implemented");
     }
 
     /**
@@ -35,7 +40,6 @@ public class ChessBoard {
      */
     public ChessPiece getPiece(ChessPosition position) {
         return board[position.getRow() - 1][position.getColumn() - 1];
-//        throw new RuntimeException("Not implemented");
     }
 
     /**
@@ -43,15 +47,61 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        this.board = new ChessPiece[8][8];
+        this.locations = new boolean[8][8];
+        ArrayList<ChessPiece.PieceType> firstRow = new ArrayList<>();
+        firstRow.add(ChessPiece.PieceType.ROOK);
+        firstRow.add(ChessPiece.PieceType.KNIGHT);
+        firstRow.add(ChessPiece.PieceType.BISHOP);
+        firstRow.add(ChessPiece.PieceType.QUEEN);
+        firstRow.add(ChessPiece.PieceType.KING);
+        firstRow.add(ChessPiece.PieceType.BISHOP);
+        firstRow.add(ChessPiece.PieceType.KNIGHT);
+        firstRow.add(ChessPiece.PieceType.ROOK);
+        int i = 0;
+        for(ChessPiece.PieceType piece : firstRow){
+            board[7][i] = new ChessPiece(ChessGame.TeamColor.BLACK, piece);
+            board[6][i] = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN);
+            board[0][i] = new ChessPiece(ChessGame.TeamColor.WHITE, piece);
+            board[1][i] = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+            i++;
+
+
+        }
     }
 
-    public boolean isPieceOnSquare(ChessPosition position){
-        return locations[position.getRow() - 1][position.getColumn() - 1];
+
+    public boolean isPieceOnSquare(int row, int col){
+        try{
+            return locations[row - 1][col - 1];
+        } catch (IndexOutOfBoundsException e){
+            return false;
+        }
+
     }
 
     public ChessPiece[][] getBoard(){
         return this.board;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof ChessBoard that)) {
+            return false;
+        }
+        return Objects.deepEquals(board, that.board);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(board);
+    }
+
+    @Override
+    public String toString() {
+        return "ChessBoard{" +
+                "board=" + Arrays.toString(board) +
+                '}';
     }
 }
 
